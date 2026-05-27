a6afc7d52dafca06a4baa43e46fa6aab294f93a0
zhi33333 <shan82990@gmail.com>
2026-05-27 22:35:36 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace 棋排類遊戲
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblStatus.Location = new System.Drawing.Point(0, 80);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(580, 40);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "目前玩家：X";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn00
+            // 
+            this.btn00.BackColor = System.Drawing.Color.White;
+            this.btn00.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn00.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn00.Location = new System.Drawing.Point(100, 150);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(120, 120);
+            this.btn00.TabIndex = 1;
+            this.btn00.UseVisualStyleBackColor = false;
+            // 
+            // btn01
+            // 
+            this.btn01.BackColor = System.Drawing.Color.White;
+            this.btn01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn01.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn01.Location = new System.Drawing.Point(230, 150);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(120, 120);
+            this.btn01.TabIndex = 2;
+            this.btn01.UseVisualStyleBackColor = false;
+            // 
+            // btn02
+            // 
+            this.btn02.BackColor = System.Drawing.Color.White;
+            this.btn02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn02.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn02.Location = new System.Drawing.Point(360, 150);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(120, 120);
+            this.btn02.TabIndex = 3;
+            this.btn02.UseVisualStyleBackColor = false;
+            // 
+            // btn10
+            // 
+            this.btn10.BackColor = System.Drawing.Color.White;
+            this.btn10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn10.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn10.Location = new System.Drawing.Point(100, 280);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(120, 120);
+            this.btn10.TabIndex = 4;
+            this.btn10.UseVisualStyleBackColor = false;
+            // 
+            // btn11
+            // 
+            this.btn11.BackColor = System.Drawing.Color.White;
+            this.btn11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn11.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn11.Location = new System.Drawing.Point(230, 280);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(120, 120);
+            this.btn11.TabIndex = 5;
+            this.btn11.UseVisualStyleBackColor = false;
+            // 
+            // btn12
+            // 
+            this.btn12.BackColor = System.Drawing.Color.White;
+            this.btn12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn12.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn12.Location = new System.Drawing.Point(360, 280);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(120, 120);
+            this.btn12.TabIndex = 6;
+            this.btn12.UseVisualStyleBackColor = false;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn20
+            // 
+            this.btn20.BackColor = System.Drawing.Color.White;
+            this.btn20.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn20.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn20.Location = new System.Drawing.Point(100, 410);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(120, 120);
+            this.btn20.TabIndex = 7;
+            this.btn20.UseVisualStyleBackColor = false;
+            // 
+            // btn21
+            // 
+            this.btn21.BackColor = System.Drawing.Color.White;
+            this.btn21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn21.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn21.Location = new System.Drawing.Point(230, 410);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(120, 120);
+            this.btn21.TabIndex = 8;
+            this.btn21.UseVisualStyleBackColor = false;
+            // 
+            // btn22
+            // 
+            this.btn22.BackColor = System.Drawing.Color.White;
+            this.btn22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn22.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn22.Location = new System.Drawing.Point(360, 410);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(120, 120);
+            this.btn22.TabIndex = 9;
+            this.btn22.UseVisualStyleBackColor = false;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnRestart.Location = new System.Drawing.Point(200, 560);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(180, 50);
+            this.btnRestart.TabIndex = 10;
+            this.btnRestart.Text = "重新開始";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.BtnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 653);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Controls.Add(this.lblStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "棋牌類遊戲";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+using System.Media;
+
+namespace 棋排類遊戲
+{
+    public partial class Form1 : Form
+    {
+        string currentPlayer = "X";
+        bool gameOver = false;
+        Button[,] buttons;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttons = new Button[3, 3]
+            {
+                { btn00, btn01, btn02 },
+                { btn10, btn11, btn12 },
+                { btn20, btn21, btn22 }
+            };
+
+            foreach (Button btn in buttons)
+            {
+                btn.Click += Cell_Click;
+                btn.BackColor = Color.White;
+                btn.ForeColor = Color.Black;
+
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor = Color.Black;
+                btn.FlatAppearance.BorderSize = 1;
+            }
+
+            btnRestart.Click += BtnRestart_Click;
+
+            lblStatus.Text = "目前玩家：X";
+        }
+        private void Cell_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+                return;
+
+            Button clickedButton = sender as Button;
+
+            if (clickedButton.Text != "")
+                return;
+
+            clickedButton.Text = currentPlayer;
+
+            if (currentPlayer == "X")
+            {
+                clickedButton.ForeColor = Color.RoyalBlue;
+            }
+            else
+            {
+                clickedButton.ForeColor = Color.Crimson;
+            }
+
+            // 點格子音效
+            SystemSounds.Beep.Play();
+
+            if (CheckWinner())
+            {
+                lblStatus.Text = "玩家 " + currentPlayer + " 獲勝！";
+
+                // 獲勝音效
+                SystemSounds.Asterisk.Play();
+
+                gameOver = true;
+                return;
+            }
+
+            if (CheckDraw())
+            {
+                lblStatus.Text = "平手！";
+
+                // 平手音效
+                SystemSounds.Exclamation.Play();
+
+                gameOver = true;
+                return;
+            }
+
+            if (currentPlayer == "X")
+            {
+                currentPlayer = "O";
+            }
+            else
+            {
+                currentPlayer = "X";
+            }
+
+            lblStatus.Text = "目前玩家：" + currentPlayer;
+        }
+
+        private bool CheckWinner()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[i, 0].Text != "" &&
+                    buttons[i, 0].Text == buttons[i, 1].Text &&
+                    buttons[i, 1].Text == buttons[i, 2].Text)
+                {
+                    HighlightWinner(buttons[i, 0], buttons[i, 1], buttons[i, 2]);
+                    return true;
+                }
+
+                if (buttons[0, i].Text != "" &&
+                    buttons[0, i].Text == buttons[1, i].Text &&
+                    buttons[1, i].Text == buttons[2, i].Text)
+                {
+                    HighlightWinner(buttons[0, i], buttons[1, i], buttons[2, i]);
+                    return true;
+                }
+            }
+
+            if (buttons[0, 0].Text != "" &&
+                buttons[0, 0].Text == buttons[1, 1].Text &&
+                buttons[1, 1].Text == buttons[2, 2].Text)
+            {
+                HighlightWinner(buttons[0, 0], buttons[1, 1], buttons[2, 2]);
+                return true;
+            }
+
+            if (buttons[0, 2].Text != "" &&
+                buttons[0, 2].Text == buttons[1, 1].Text &&
+                buttons[1, 1].Text == buttons[2, 0].Text)
+            {
+                HighlightWinner(buttons[0, 2], buttons[1, 1], buttons[2, 0]);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckDraw()
+        {
+            foreach (Button btn in buttons)
+            {
+                if (btn.Text == "")
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void HighlightWinner(Button b1, Button b2, Button b3)
+        {
+            b1.BackColor = Color.LightGreen;
+            b2.BackColor = Color.LightGreen;
+            b3.BackColor = Color.LightGreen;
+        }
+
+        private void BtnRestart_Click(object sender, EventArgs e)
+        {
+            foreach (Button btn in buttons)
+            {
+                btn.Text = "";
+                btn.BackColor = Color.White;
+                btn.ForeColor = Color.Black;
+
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor = Color.Black;
+                btn.FlatAppearance.BorderSize = 1;
+            }
+
+            currentPlayer = "X";
+            gameOver = false;
+            lblStatus.Text = "目前玩家：X";
+        }
+    }
+}
